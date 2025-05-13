d50f72d3e45c260b43c5032434b7ee6e2ab00e3b
Paula Schlötermann <paula.schloetermann@iteratec.com>
2025-05-13 09:22:53 +0200

    Finishing LA

--- biocentral_server/pred/models/light_attention.py
+++ biocentral_server/pred/models/light_attention.py
@@ -1,14 +1,15 @@
 import torch
 import numpy as np
 
-from ..utils import to_cpu
+from ..utils import to_cpu, get_batched_data
 from base_model import BaseModel
 from ..utils import load_onnx_model
 from ..metadata_endpoint import get_metadata
 
 
 class LightAttention(BaseModel):
-    def __init__(self):
+    def __init__(self, batch_size):
+        super().__init__(batch_size=batch_size)
         self.la_subcell = load_onnx_model(model_name='la_subcell')
         self.la_mem = load_onnx_model(model_name='la')
         self.device = ""
@@ -31,30 +32,31 @@ class LightAttention(BaseModel):
         }
 
     def _prepare_inputs(self, embeddings):
-        ort_inputs = {'input': embeddings.numpy(),
-                      'mask': attention_mask.numpy()}
-        return ort_inputs
+        return get_batched_data(batch_size=self.batch_size, data=embeddings.values(), mask=True)
 
     def predict(self, embeddings):
         # TODO: get rid of to_numpy, from numpy...
         inputs = self._prepare_inputs(embeddings=embeddings)
-        subcell_Yhat = self.la_subcell.run(None, inputs)
-        subcell_Yhat = torch.from_numpy(np.float32(np.stack(subcell_Yhat[0])))
-        subcell_Yhat = to_cpu(torch.max(subcell_Yhat, dim=1)[1]).astype(np.byte)
+        embedding_ids = embeddings.keys()
+        results = []
+        for batch in inputs:
+            subcell_Yhat = self.la_subcell.run(None, batch)
+            subcell_Yhat = torch.from_numpy(np.float32(np.stack(subcell_Yhat[0])))
+            subcell_Yhat = to_cpu(torch.max(subcell_Yhat, dim=1)[1]).astype(np.byte)
 
-        la_mem_Yhat = self.la_mem.run(None, inputs)
-        la_mem_Yhat = torch.from_numpy(np.float32(np.stack(la_mem_Yhat[0])))
-        la_mem_Yhat = to_cpu(torch.max(la_mem_Yhat, dim=1)[1]).astype(np.byte)
+            la_mem_Yhat = self.la_mem.run(None, batch)
+            la_mem_Yhat = torch.from_numpy(np.float32(np.stack(la_mem_Yhat[0])))
+            la_mem_Yhat = to_cpu(torch.max(la_mem_Yhat, dim=1)[1]).astype(np.byte)
+            batch_result = [{'subcell': subcell, 'mem': mem} for subcell, mem in zip(subcell_Yhat, la_mem_Yhat)]
+            results.extend(batch_result)
+        return self._post_process(model_output=results, embedding_ids=embedding_ids)
 
-        return self._post_process(model_output={
-            'subcell': subcell_Yhat,
-            'mem': la_mem_Yhat
-        })
+    def _post_process(self, model_output, embedding_ids):
+        formatted_predictions = {}
+        for i, pred in enumerate(model_output):
+            formatted_predictions[embedding_ids[i]] =  {
+                'subcell': self.class2label_subcell[pred['subcell']],
+                'mem': self.class2label_mem[pred['mem']]
+            }
+        return formatted_predictions
 
-    def _post_process(self, model_output):
-        # mehrere Sequence Ids -> weil mehrere Sequences in embeddings
-        return {sequence_id: {
-            'subcell': '\n'.join([self.class2label_subcell[pred] for pred in model_output['subcell']]),
-            'mem': '\n'.join([self.class2label_mem[pred] for pred in model_output['mem']])
-        }
-        }
