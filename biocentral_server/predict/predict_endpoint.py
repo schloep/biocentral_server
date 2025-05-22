ee045c5110c3f40adcbf4e0375f839550d53dd5e
Sebastian <sebastian.franz@tum.de>
2025-05-22 16:28:13 +0200

    Adding sequence verification for predict endpoint
    string, min_seq_length, max_seq_length
    
    Signed-off-by: Sebastian <sebastian.franz@tum.de>

--- biocentral_server/predict/predict_endpoint.py
+++ biocentral_server/predict/predict_endpoint.py
@@ -9,6 +9,20 @@ from ..server_management import TaskManager
 
 prediction_service_route = Blueprint("prediction_service_predict", __name__)
 
+def verify_sequences(sequence_input: dict[str, str]) -> str:
+    min_seq_length = 7
+    max_seq_length = 5000  # TODO Make this configurable
+    for seq_id, seq in sequence_input.items():
+        if not isinstance(seq, str):
+            return f"{seq_id} is not a string"
+        if len(seq) < min_seq_length:
+            return f"{seq_id} is too short, min_seq_length={min_seq_length}, max_seq_length={max_seq_length}"
+        elif len(seq) > max_seq_length:
+            return f"{seq_id} is too long, max_seq_length={max_seq_length}, min_seq_length={min_seq_length}"
+
+    return ""
+
+
 # Endpoint for ProtSpace dimensionality reduction methods for sequences
 @prediction_service_route.route('/prediction_service/predict', methods=['POST'])
 def predict():
@@ -19,9 +33,14 @@ def predict():
     if any(model_name not in model_metadata.keys() for model_name in model_names):
         return jsonify({"error": "A requested model was not found!"})
 
+    sequence_input = request_data.sequence_input
+    sequence_verification_error = verify_sequences(sequence_input)
+    if sequence_verification_error != "":
+        return jsonify({"error": sequence_verification_error})
+
     models = filter_models(model_names=model_names)
     prediction_task = MultiPredictionTask(models=models,
-                                          sequence_input=request_data.sequence_input,
+                                          sequence_input=sequence_input,
                                           batch_size=request_data.batch_size)
     task_id = TaskManager().add_task(prediction_task)
     return jsonify({"task_id": task_id})
